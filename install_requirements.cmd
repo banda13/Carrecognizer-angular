6cad71f477fcb38f65d9a6c80a16a7211d80e66a
Szabó András <szabag@gmail.com>
2019-03-20 15:34:40 +0100

    installing all node packages together

---
+++ install_requirements.cmd
@@ -0,0 +1,18 @@
+echo Installing angular client..
+call npm install --global @angular/cli
+
+echo Installing node packages..
+call npm install bootstrap --save
+
+call npm install ng2-file-upload --save
+
+call npm install ngx-toastr --save
+call npm install @angular/animations --save
+
+call npm i angular-bootstrap-md --save
+
+call npm install @angular/http --save
+
+call npm install --save ng2-charts
+
+echo All packages successfully installed!
